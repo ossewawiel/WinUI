dd07ed70d1a8ece571e0a07f57708d807b8fbccf
ossewawiel <m.pretorius@optimus2020.com>
2020-05-22 10:42:39 +0200

    some toolbar work

--- WinUI/win_toolbar_command.cpp
+++ WinUI/win_toolbar_command.cpp
@@ -1,2 +1,18 @@
 #include "win_menu_command.h"
 #include "win_toolbar_command.h"
+
+win_toolbar_command::win_toolbar_command(
+	NN(win_toolbar*) parent
+	, NN(win_menu_command*) mnu_cmd
+	, std::wstring const& tooltip
+	, std::wstring const& text
+	, UINT bmp_id) :
+	_parent{ parent },
+	_menu_command{ mnu_cmd },
+	_tooltip{ tooltip },
+	_text{ text.empty() ? mnu_cmd->text() : text },
+	_bmp_id{ bmp_id == 0 ? mnu_cmd->bmp_id() : bmp_id }
+{
+	_menu_command->set_on_command_checked(std::bind(&win_toolbar_command::on_command_checked, this, std::placeholders::_1));
+	_menu_command->set_on_command_enabled(std::bind(&win_toolbar_command::on_command_enabled, this, std::placeholders::_1));
+}
--- WinUI/win_toolbar_command.h
+++ WinUI/win_toolbar_command.h
@@ -13,16 +13,7 @@ public:
 		, NN(win_menu_command*) mnu_cmd
 		, std::wstring const& tooltip
 		, std::wstring const& text = L""
-		, UINT bmp_id = 0):
-		_parent{parent},
-		_menu_command{ mnu_cmd },
-		_tooltip{ tooltip },
-		_text{ text.empty() ? mnu_cmd->text() : L"" },
-		_bmp_id{ bmp_id == 0 ? mnu_cmd->bmp_id() : 0 }
-	{
-		_menu_command->set_on_command_checked(std::bind(&win_toolbar_command::on_command_checked, this, std::placeholders::_1));
-		_menu_command->set_on_command_enabled(std::bind(&win_toolbar_command::on_command_enabled, this, std::placeholders::_1));
-	}
+		, UINT bmp_id = 0);
 
 	//PROPERTIES//
 
